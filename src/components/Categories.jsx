534387984c1775069ab5710fc68ba74e5159749a
Vadim Hmys <vadimhmyz@gmail.com>
2024-04-26 22:53:41 +0300

    implement switching list's elements

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 export default function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((c, i) => (
+          <li key={i} className={activeIndex === i ? 'active' : ''} onClick={() => handleClick(i)}>
+            {c}
+          </li>
+        ))}
       </ul>
     </div>
   );
