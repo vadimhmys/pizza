5d5ff8354f20e200ead4d763ad9cbe65476f118d
Vadim Hmys <vadimhmyz@gmail.com>
2024-04-26 21:05:07 +0300

    finish lesson #3

--- src/App.js
+++ src/App.js
@@ -7,23 +7,18 @@ import './scss/app.scss';
 function App() {
   return (
     <div className="App">
-      <div class="wrapper">
+      <div className="wrapper">
         <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
               <Categories />
               <Sort />
             </div>
-            <h2 class="content__title">Все пиццы</h2>
-            <div class="content__items">
-              <Pizza/>
-              <Pizza/>
-              <Pizza/>
-              <Pizza/>
-              <Pizza/>
-              <Pizza/>
-              <Pizza/>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <Pizza title="Чизбургер-пицца" price={395} />
+              <Pizza title="hello" price={400} />
             </div>
           </div>
         </div>
