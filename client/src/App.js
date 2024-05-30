c31abdea37b22e246075404c3584512a6fd17eb5
Vadim Hmyz <vadimhmyz@gmail.com>
2024-05-30 23:14:27 +0300

    feat: add simple routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,22 @@
-import { useEffect, useState } from 'react';
-import Categories from './components/Categories';
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
-import Pizza from './components/Pizza';
-import Sort from './components/Sort';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 import './scss/app.scss';
-import PizzaLoader from './components/Pizza/PizzaLoader';
+import Cart from './pages/Cart';
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch('http://localhost:7000/api/pizza/getall')
-      .then((res) => res.json())
-      .then((arr) => {
-        setItems(arr);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
-          <div className="container">
-            <div className="content__top">
-              <Categories />
-              <Sort />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              {isLoading
-                ? [...new Array(6)].map((_, index) => <PizzaLoader key={index} />)
-                : items.map((i) => (
-                    <Pizza
-                      key={i.id}
-                      title={i.title}
-                      price={i.price}
-                      imageUrl={i.imageUrl}
-                      sizes={i.sizes}
-                      types={i.types}
-                    />
-                  ))}
-            </div>
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+import Categories from '../components/Categories';
+import Pizza from '../components/Pizza';
+import Sort from '../components/Sort';
+import PizzaLoader from '../components/Pizza/PizzaLoader';
+
+export default function Home() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:7000/api/pizza/getall')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          {isLoading
+            ? [...new Array(6)].map((_, index) => <PizzaLoader key={index} />)
+            : items.map((i) => (
+                <Pizza
+                  key={i.id}
+                  title={i.title}
+                  price={i.price}
+                  imageUrl={i.imageUrl}
+                  sizes={i.sizes}
+                  types={i.types}
+                />
+              ))}
+        </div>
+      </div>
+    </>
+  );
+}
