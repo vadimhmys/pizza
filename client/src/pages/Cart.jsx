12778c8c77a5ecf83fd01882ec60624cde0f85d6
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-05 22:16:45 +0300

    add route between home and cart pages

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 export default function Cart() {
   return (
     <div class="container container--cart">
@@ -378,7 +380,7 @@ export default function Cart() {
             </span>
           </div>
           <div class="cart__bottom-buttons">
-            <a href="/" class="button button--outline button--add go-back-btn">
+            <Link to="/" class="button button--outline button--add go-back-btn">
               <svg
                 width="8"
                 height="14"
@@ -395,7 +397,7 @@ export default function Cart() {
               </svg>
 
               <span>Вернуться назад</span>
-            </a>
+            </Link>
             <div class="button pay-btn">
               <span>Оплатить сейчас</span>
             </div>
