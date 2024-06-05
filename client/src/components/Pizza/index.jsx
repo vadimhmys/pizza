dd29cfe2df61939c9365f7e23e51dbff05897dde
Vadim Hmyz <vadimhmyz@gmail.com>
2024-06-05 21:16:07 +0300

    feat: create response layout

--- client/src/components/Pizza/index.jsx
+++ client/src/components/Pizza/index.jsx
@@ -16,34 +16,36 @@ export default function Pizza({ title, price, imageUrl, sizes, types }) {
   };
 
   return (
-    <div className="pizza-block">
-      <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
-      <h4 className="pizza-block__title">{title}</h4>
-      <div className="pizza-block__selector">
-        <ul>
-          {types.map((t, index) => (
-            <li
-              key={t}
-              className={index === activeTypeIndex ? 'active' : ''}
-              onClick={() => onTypeClick(index)}>
-              {pizzaTypes[t]}
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {sizes.map((s, index) => (
-            <li
-              key={s}
-              className={index === activeSizeIndex ? 'active' : ''}
-              onClick={() => onSizeClick(index)}>
-              {s} см.
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="pizza-block__bottom">
-        <div className="pizza-block__price">от {price} ₽</div>
-        <Button />
+    <div className="pizza-block-wrapper">
+      <div className="pizza-block">
+        <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
+        <h4 className="pizza-block__title">{title}</h4>
+        <div className="pizza-block__selector">
+          <ul>
+            {types.map((t, index) => (
+              <li
+                key={t}
+                className={index === activeTypeIndex ? 'active' : ''}
+                onClick={() => onTypeClick(index)}>
+                {pizzaTypes[t]}
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {sizes.map((s, index) => (
+              <li
+                key={s}
+                className={index === activeSizeIndex ? 'active' : ''}
+                onClick={() => onSizeClick(index)}>
+                {s} см.
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="pizza-block__bottom">
+          <div className="pizza-block__price">от {price} ₽</div>
+          <Button />
+        </div>
       </div>
     </div>
   );
