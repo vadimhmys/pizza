68d798416b84f26c028ea0130a0b4f364d9495f7
Vadim Hmyz <vadimhmyz@gmail.com>
2024-07-01 21:23:27 +0300

    introduction to reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/slices/filterSlice';
 
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -12,10 +14,25 @@ export const SearchContext = React.createContext();
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <div className="wrapper">
-        <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+      <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}>
           <Header />
           <div className="content">
             <Routes>
@@ -24,7 +41,7 @@ function App() {
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
-        </SearchContext.Provider>
+        </SearchContext.Provider> */}
       </div>
     </div>
   );
