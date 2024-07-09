f9f2b79007879f301caaab2de329225ff2718e2f
Vadim Hmyz <vadimhmyz@gmail.com>
2024-07-09 11:02:28 +0300

    hide popup when clicking outside the sorting block

--- client/src/components/Sort.jsx
+++ client/src/components/Sort.jsx
@@ -15,14 +15,28 @@ export const list = [
 export default function Sort() {
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
+  const sortRef = React.useRef(null);
   const [open, setOpen] = React.useState(false);
   const onClickListItem = (obj) => {
     dispatch(setSort(obj));
     setOpen(false);
   };
 
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+        setOpen(false);
+        console.log('click outside');
+      }
+    };
+
+    document.body.addEventListener('click', handleClickOutside);
+
+    return () => document.body.removeEventListener('click', handleClickOutside);
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
