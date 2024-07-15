a93657d8d0db477d53fb7951ffe753161eb6b982
Vadim Hmyz <vadimhmyz@gmail.com>
2024-07-15 23:28:46 +0300

    add tsconfig

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
