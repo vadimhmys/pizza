00d91405fcb5417e74fc20c02b12db1f6f798eb3
Vadim Hmyz <vadimhmyz@gmail.com>
2024-07-16 10:16:34 +0300

    typify FullPizza

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,13 +7,17 @@ import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+const rootElem = document.getElementById('root');
+
+if (rootElem) {
+  const root = ReactDOM.createRoot(rootElem);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+}
--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
 
-
 export default function MainLayout() {
   return (
     <div className="App">
